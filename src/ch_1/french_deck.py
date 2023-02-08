6944e9c337e93defe8f5b89d0c8b06e48f240752
Paul Tuck <paul.tuck@oxon.org>
2023-02-08 08:54:16 +0700

    "2023-02-08_08:54_ICT+7"

--- src/ch_1/french_deck.py
+++ src/ch_1/french_deck.py
@@ -1,18 +1,22 @@
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
+
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self._cards = [Card(rank, suit)
+                       for suit in self.suits
+                       for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
 
-    def __getitem__(self,position):
+    def __getitem__(self, position):
         return self._cards[position]
 
 
@@ -21,3 +25,13 @@ print(beer_card)
 
 deck = FrenchDeck()
 print(str(len(deck)) + " cards in the deck")
+
+print(deck[0])
+print(deck[-1])
+
+print("Random card: " + str(choice(deck)))
+
+print(deck[12:13])          # Slicing not working properly
+for card in deck[10:13]:
+    print(card)
+
