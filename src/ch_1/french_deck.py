0386a954162954b5326d616d72e87d2913ab03d9
Paul Tuck <paul.tuck@oxon.org>
2023-02-09 01:22:01 +0700

    "2023-02-09_01:22_ICT+7"

--- src/ch_1/french_deck.py
+++ src/ch_1/french_deck.py
@@ -1,17 +1,15 @@
 import collections
 from random import choice
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = collections.namedtuple("Card", ["rank", "suit"])
 
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit)
-                       for suit in self.suits
-                       for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
@@ -20,10 +18,13 @@ class FrenchDeck:
         return self._cards[position]
 
 
-beer_card = Card('7', 'diamonds')
+beer_card = Card("7", "diamonds")
 print(beer_card)
 
 deck = FrenchDeck()
+
+print(deck.ranks)
+print(deck.suits)
 print(str(len(deck)) + " cards in the deck")
 
 print(deck[0])
@@ -31,7 +32,24 @@ print(deck[-1])
 
 print("Random card: " + str(choice(deck)))
 
-print(deck[12:13])          # Slicing not working properly
-for card in deck[10:13]:
+print(deck[12:13])  # Slicing not working properly
+# for card in reversed(deck):
+#    print(card)
+
+print(Card("7", "beasts") in deck)
+
+# SORTING
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
     print(card)
 
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+# The deck can't be suffled because it's immutable
